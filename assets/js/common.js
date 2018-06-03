4ca0748817ed7838beb407e4ed207155e0207332
zhanghuiblock <hui.zhang@blockin.com>
2018-06-03 23:19:15 +0800

    1

--- assets/js/common.js
+++ assets/js/common.js
@@ -11,4 +11,24 @@ function setContentHeight() {
   var screenHeight = $(window).height();
   var headerHeight = $(".header").outerHeight();
   $(".content").height(screenHeight - headerHeight);
+  // maxHeight();
+}
+function maxHeight() {
+  // ????存在问题
+  var h1,h2,timer;
+  clearTimeout(timer);
+  $(".news-list,.news-side").css('height','auto');
+  var timer=setTimeout(function() {
+    h1 = $(".news-list").outerHeight();
+    h2 = $(".news-side").outerHeight();
+    console.log(h1, h2);
+    if(h1>h2){
+      $(".news-side").height(h1)
+      $(".news-list").height(h1)
+    }else{
+      $(".news-side").height(h2)
+      $(".news-list").height(h2)
+    }
+  }, 300);
+  
 }
